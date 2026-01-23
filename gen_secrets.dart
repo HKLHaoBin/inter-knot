a70293361ad1821066d29a3121d1ed7a7696fd50
HKLHaoBin <we3q@qq.com>
2026-01-24 01:09:04 +0800

    ci: 移除PEM相关配置及代码
    移除不再使用的PEM密钥相关配置，包括工作流文件和代码生成脚本中的PEM环境变量和引用

--- gen_secrets.dart
+++ gen_secrets.dart
@@ -1,9 +1,8 @@
 import 'dart:io';
 
 void main() {
-  final pem = Platform.environment['PEM'];
   final clientId = Platform.environment['CLIENT_ID'];
   File('lib/secret.dart').writeAsString(
-    "import 'dart:convert';\nfinal pem = String.fromCharCodes(base64Decode('$pem'));\nfinal clientId = String.fromCharCodes(base64Decode('$clientId'));",
+    "import 'dart:convert';\nfinal clientId = String.fromCharCodes(base64Decode('$clientId'));",
   );
 }
